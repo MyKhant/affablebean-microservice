bbc71b113104913972c0c1e218181a2cf1ca6892
MyKhant <myokhantkyaw148@gmail.com>
2023-02-09 15:53:17 +0630

    Upload microservice security

---
+++ api-security/src/main/java/com/example/apisecurity/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,26 @@
+package com.example.apisecurity.interceptor;
+
+import com.example.apisecurity.exception.NoBearToken;
+import com.example.apisecurity.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AuthorizationInterceptor implements HandlerInterceptor {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String handlerString = request.getHeader("Authorization");
+        if (handlerString == null || !handlerString.startsWith("Bearer ")){
+            throw new NoBearToken();
+        }
+        request.setAttribute("user",
+                userService.getUserFromToken(handlerString.substring(7)));
+        return true;
+    }
+}
